e4ae6d49d784c03780f45242dfbf0700a44a0ff2
Alexey Skriptsov <alexey.skriptsov@dopsdigital.com>
2020-12-09 11:28:37 +0000

    update bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source  = "app.terraform.io/alexeyskriptsov-training/s3-bucket/aws"
   version = "1.17.0"
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-testtttttttttttt"
   acl    = "private"
 
   versioning = {
